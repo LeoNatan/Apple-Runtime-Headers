1887dc3386f982f87c8029af17440a6324b527f0
Leo Natan <leo.natan@outlook.com>
2018-10-15 16:15:14 +0300

    Include internal frameworks

--- generate_headers.sh
+++ generate_headers.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 
 rm -fr iOS
 rm -fr macOS
@@ -21,112 +21,108 @@ mkdir -p macOS/Frameworks
 mkdir -p macOS/PrivateFrameworks
 mkdir -p macOS/usr/lib
 
+visitFrameworkOrApp() {
+	FRAMEWORK="$1"
+	ARCH="$2"
+	BASEPATH="$3"
+	
+	if [[ -f "$FRAMEWORK" ]]; then
+		FRAMEWORK_BASENAME="${${$(basename "$FRAMEWORK")%.*}##lib}"
+	else
+		FRAMEWORK_BASENAME="$(basename "$FRAMEWORK")"
+	fi
+		
+	echo -e "\033[1;34mProcessing $FRAMEWORK\033[0m"
+	./class-dump --arch "$ARCH" -H -o "$BASEPATH/$FRAMEWORK_BASENAME" "$FRAMEWORK"
+		
+	if [[ -d "$FRAMEWORK/Frameworks" ]]; then
+		for INNER_FRAMEWORK in "${FRAMEWORK}"/Frameworks/* ; do
+			visitFrameworkOrApp "$INNER_FRAMEWORK" "$ARCH" "$BASEPATH/$FRAMEWORK_BASENAME"/Frameworks
+		done		
+	fi
+}
+
 iOS() {
 	# iOS
 
 	for FRAMEWORK in "$(xcode-select -p)"/Platforms/iPhoneOS.platform/Developer/Library/CoreSimulator/Profiles/Runtimes/iOS.simruntime/Contents/Resources/RuntimeRoot/System/Library/Frameworks/*.framework ; do
-	  echo -e "\033[1;34mProcessing $(basename $FRAMEWORK)\033[0m"
-	  ./class-dump --arch x86_64 -H -o iOS/Frameworks/$(basename "$FRAMEWORK") "$FRAMEWORK"
+		visitFrameworkOrApp "$FRAMEWORK" x86_64 iOS/Frameworks/
 	done
 
 	for FRAMEWORK in "$(xcode-select -p)"/Platforms/iPhoneOS.platform/Developer/Library/CoreSimulator/Profiles/Runtimes/iOS.simruntime/Contents/Resources/RuntimeRoot/System/Library/PrivateFrameworks/*.framework ; do
-	  printf >&2 "\033[1;31mProcessing $(basename $FRAMEWORK)\033[0m\n"
-	  ./class-dump --arch x86_64 -H -o iOS/PrivateFrameworks/$(basename "$FRAMEWORK") "$FRAMEWORK"
+		visitFrameworkOrApp "$FRAMEWORK" x86_64 iOS/PrivateFrameworks/
 	done
-
+	
 	for FRAMEWORK in "$(xcode-select -p)"/Platforms/iPhoneOS.platform/Developer/Library/CoreSimulator/Profiles/Runtimes/iOS.simruntime/Contents/Resources/RuntimeRoot/usr/lib/*.dylib ; do
-	  echo -e "\033[1;34mProcessing $(basename $FRAMEWORK)\033[0m"
-	  FILE=$(basename "$FRAMEWORK")
-	  ./class-dump --arch x86_64 -H -o iOS/usr/lib/${FILE%.*} "$FRAMEWORK"
+		visitFrameworkOrApp "$FRAMEWORK" x86_64 iOS/usr/lib/
 	done
-
+	
 	for FRAMEWORK in "$(xcode-select -p)"/Platforms/iPhoneOS.platform/Developer/Library/CoreSimulator/Profiles/Runtimes/iOS.simruntime/Contents/Resources/RuntimeRoot/usr/lib/system/*.dylib ; do
-	  echo -e "\033[1;34mProcessing $(basename $FRAMEWORK)\033[0m"
-	  FILE=$(basename "$FRAMEWORK")
-	  ./class-dump --arch x86_64 -H -o iOS/usr/lib/${FILE%.*} "$FRAMEWORK"
+		visitFrameworkOrApp "$FRAMEWORK" x86_64 iOS/usr/lib/
 	done
 }
 
 watchOS() {
 	# watchOS
-
+	
 	for FRAMEWORK in "$(xcode-select -p)"/Platforms/WatchOS.platform/Developer/Library/CoreSimulator/Profiles/Runtimes/watchOS.simruntime/Contents/Resources/RuntimeRoot/System/Library/Frameworks/*.framework ; do
-	  echo -e "\033[1;34mProcessing $(basename $FRAMEWORK)\033[0m"
-	  ./class-dump --arch i386 -H -o watchOS/Frameworks/$(basename "$FRAMEWORK") "$FRAMEWORK"
+		visitFrameworkOrApp "$FRAMEWORK" i386 watchOS/Frameworks/
 	done
 
 	for FRAMEWORK in "$(xcode-select -p)"/Platforms/WatchOS.platform/Developer/Library/CoreSimulator/Profiles/Runtimes/watchOS.simruntime/Contents/Resources/RuntimeRoot/System/Library/PrivateFrameworks/*.framework ; do
-	  printf >&2 "\033[1;31mProcessing $(basename $FRAMEWORK)\033[0m\n"
-	  ./class-dump --arch i386 -H -o watchOS/PrivateFrameworks/$(basename "$FRAMEWORK") "$FRAMEWORK"
+		visitFrameworkOrApp "$FRAMEWORK" i386 watchOS/PrivateFrameworks/
 	done
-
+	
 	for FRAMEWORK in "$(xcode-select -p)"/Platforms/WatchOS.platform/Developer/Library/CoreSimulator/Profiles/Runtimes/watchOS.simruntime/Contents/Resources/RuntimeRoot/usr/lib/*.dylib ; do
-	  echo -e "\033[1;34mProcessing $(basename $FRAMEWORK)\033[0m"
-	  FILE=$(basename "$FRAMEWORK")
-	  ./class-dump --arch i386 -H -o watchOS/usr/lib/${FILE%.*} "$FRAMEWORK"
+		visitFrameworkOrApp "$FRAMEWORK" i386 watchOS/usr/lib/
 	done
-
+	
 	for FRAMEWORK in "$(xcode-select -p)"/Platforms/WatchOS.platform/Developer/Library/CoreSimulator/Profiles/Runtimes/watchOS.simruntime/Contents/Resources/RuntimeRoot/usr/lib/system/*.dylib ; do
-	  echo -e "\033[1;34mProcessing $(basename $FRAMEWORK)\033[0m"
-	  FILE=$(basename "$FRAMEWORK")
-	  ./class-dump --arch i386 -H -o watchOS/usr/lib/${FILE%.*} "$FRAMEWORK"
+		visitFrameworkOrApp "$FRAMEWORK" i386 watchOS/usr/lib/
 	done
 }
 
 tvOS() {
 	# tvOS
-
+	
 	for FRAMEWORK in "$(xcode-select -p)"/Platforms/AppleTVOS.platform/Developer/Library/CoreSimulator/Profiles/Runtimes/tvOS.simruntime/Contents/Resources/RuntimeRoot/System/Library/Frameworks/*.framework ; do
-	  echo -e "\033[1;34mProcessing $(basename $FRAMEWORK)\033[0m"
-	  ./class-dump --arch x86_64 -H -o tvOS/Frameworks/$(basename "$FRAMEWORK") "$FRAMEWORK"
+		visitFrameworkOrApp "$FRAMEWORK" x86_64 tvOS/Frameworks/
 	done
 
 	for FRAMEWORK in "$(xcode-select -p)"/Platforms/AppleTVOS.platform/Developer/Library/CoreSimulator/Profiles/Runtimes/tvOS.simruntime/Contents/Resources/RuntimeRoot/System/Library/PrivateFrameworks/*.framework ; do
-	  printf >&2 "\033[1;31mProcessing $(basename $FRAMEWORK)\033[0m\n"
-	  ./class-dump --arch x86_64 -H -o tvOS/PrivateFrameworks/$(basename "$FRAMEWORK") "$FRAMEWORK"
+		visitFrameworkOrApp "$FRAMEWORK" x86_64 tvOS/PrivateFrameworks/
 	done
-
+	
 	for FRAMEWORK in "$(xcode-select -p)"/Platforms/AppleTVOS.platform/Developer/Library/CoreSimulator/Profiles/Runtimes/tvOS.simruntime/Contents/Resources/RuntimeRoot/usr/lib/*.dylib ; do
-	  echo -e "\033[1;34mProcessing $(basename $FRAMEWORK)\033[0m"
-	  FILE=$(basename "$FRAMEWORK")
-	  ./class-dump --arch x86_64 -H -o tvOS/usr/lib/${FILE%.*} "$FRAMEWORK"
+		visitFrameworkOrApp "$FRAMEWORK" x86_64 tvOS/usr/lib/
 	done
-
+	
 	for FRAMEWORK in "$(xcode-select -p)"/Platforms/AppleTVOS.platform/Developer/Library/CoreSimulator/Profiles/Runtimes/tvOS.simruntime/Contents/Resources/RuntimeRoot/usr/lib/system/*.dylib ; do
-	  echo -e "\033[1;34mProcessing $(basename $FRAMEWORK)\033[0m"
-	  FILE=$(basename "$FRAMEWORK")
-	  ./class-dump --arch x86_64 -H -o tvOS/usr/lib/${FILE%.*} "$FRAMEWORK"
+		visitFrameworkOrApp "$FRAMEWORK" x86_64 tvOS/usr/lib/
 	done
 }
 
 macOS() {
 	# macOS
-
+	
 	for FRAMEWORK in /System/Library/Frameworks/*.framework ; do
-	  echo -e "\033[1;34mProcessing $(basename $FRAMEWORK)\033[0m"
-	  ./class-dump --arch x86_64 -H -o macOS/Frameworks/$(basename "$FRAMEWORK") "$FRAMEWORK"
+		visitFrameworkOrApp "$FRAMEWORK" x86_64 macOS/Frameworks/
 	done
 
 	for FRAMEWORK in /System/Library/PrivateFrameworks/*.framework ; do
-	  printf >&2 "\033[1;31mProcessing $(basename $FRAMEWORK)\033[0m\n"
-	  ./class-dump --arch x86_64 -H -o macOS/PrivateFrameworks/$(basename "$FRAMEWORK") "$FRAMEWORK"
+		visitFrameworkOrApp "$FRAMEWORK" x86_64 macOS/PrivateFrameworks/
 	done
-
+	
 	for FRAMEWORK in /usr/lib/*.dylib ; do
-	  echo -e "\033[1;34mProcessing $(basename $FRAMEWORK)\033[0m"
-	  FILE=$(basename "$FRAMEWORK")
-	  ./class-dump --arch x86_64 -H -o macOS/usr/lib/${FILE%.*} "$FRAMEWORK"
+		visitFrameworkOrApp "$FRAMEWORK" x86_64 macOS/usr/lib/
 	done
-
+	
 	for FRAMEWORK in /usr/lib/system/*.dylib ; do
-	  echo -e "\033[1;34mProcessing $(basename $FRAMEWORK)\033[0m"
-	  FILE=$(basename "$FRAMEWORK")
-	  ./class-dump --arch x86_64 -H -o macOS/usr/lib/${FILE%.*} "$FRAMEWORK"
+		visitFrameworkOrApp "$FRAMEWORK" x86_64 macOS/usr/lib/
 	done
 }
 
-iOS &
-watchOS &
-tvOS &
-macOS &
-wait
+iOS
+watchOS
+tvOS
+macOS
